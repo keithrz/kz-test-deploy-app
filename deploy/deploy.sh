4ba4bf31938cef730141e62fa3530307a28d67ca
Keith Zimmerman <keithrz@gmail.com>
2019-01-19 22:34:57 -0600

    updated deploy script: remove commented out stuff

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,4 @@
-#deploy: # from https://docs.travis-ci.com/user/deployment/custom/
-#  - eval "$(ssh-agent -s)" #start the ssh agent
-#  - chmod 600 .travis/deploy_key.pem # this key should have push access
-#  - ssh-add .travis/deploy_key.pem
-#  - git remote add deploy ssh://bitnami@http://18.191.210.81/~/repos/kz-test-deploy-app
-#  - git push deploy
-
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deploy_key.pem # this key should have push access
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_key.pem
 ssh-add .travis/deploy_key.pem
-#git remote add deploy ssh://bitnami@http://18.191.210.81/~/repos/kz-test-deploy-app
-#echo before push
-#git push deploy --force
-#echo after push
 scp -rp ./* bitnami@http://18.191.210.81/~/projects/kz-test-deploy-app
-
