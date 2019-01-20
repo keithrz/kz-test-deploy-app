52ba9d4bdbdced3ce22635a7198e733c1b731ae8
Keith Zimmerman <keithrz@gmail.com>
2019-01-19 22:04:22 -0600

    updated deploy script: added debugging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,4 +9,6 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key.pem # this key should have push access
 ssh-add .travis/deploy_key.pem
 git remote add deploy ssh://bitnami@http://18.191.210.81/~/repos/kz-test-deploy-app
+echo before push
 git push deploy --force
+echo after push
