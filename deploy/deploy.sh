e6112e3d372209f9d1380ec00b21085f06ce3f44
Keith Zimmerman <keithrz@gmail.com>
2019-01-20 00:35:58 -0600

    Set up Travis SSH deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,1 @@
-eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy_key.pem
-ssh-add .travis/deploy_key.pem
-scp -rp ./* bitnami@http://18.191.210.81/~/projects/kz-test-deploy-app
+#rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build bitnami@18.191.210.81/~/projects/kz-test-deploy-app
