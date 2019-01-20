33d53186fa9eb033b285859f7c7a8d705b8c539e
Keith Zimmerman <keithrz@gmail.com>
2019-01-19 22:01:38 -0600

    updated deploy script: made git push a force push

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,4 +9,4 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy_key.pem # this key should have push access
 ssh-add .travis/deploy_key.pem
 git remote add deploy ssh://bitnami@http://18.191.210.81/~/repos/kz-test-deploy-app
-git push deploy
+git push deploy --force
